f78dbf89f4f1f723699a182d4ba711e551eebbc5
lucasmartins2506 <lucasnalua25@gmail.com>
2019-09-04 20:25:50 -0300

    Exercicio - 3

---
+++ exercicio3-4.ts
@@ -0,0 +1,13 @@
+{
+    let salario: number = 1500.98;
+
+    if (salario % 2 == 0 )
+    {
+        console.log(`Este número é par `);
+    }
+    else
+    {
+        console.log(`Este número é impar`);
+    }
+
+}
