67b46617f8e79a07b51e0d7f7ac54e25e9afc7dd
lucasmartins2506 <lucasnalua25@gmail.com>
2019-09-04 20:42:06 -0300

    Exercicio - 5

---
+++ exercicio5.ts
@@ -0,0 +1,8 @@
+{
+    let comprimento:number = 15;
+    let largura : number = 5;
+
+    let area =(comprimento:number,largura : number) => (comprimento * largura)
+
+    console.log( `A área é: ${area(comprimento,largura)}`);
+}
