eeb52877068d4553ebbb9eb51be285424dc8cd44
lucasmartins2506 <lucasnalua25@gmail.com>
2019-09-04 20:36:17 -0300

    Exercicio - 4

---
+++ exercicio-4.ts
@@ -0,0 +1,18 @@
+{
+    /*let salario: number = 1500.98;
+
+    if  (salario % 2 == 0 )
+    {
+        console.log(`Este número é par `);
+    }
+    else
+    {
+        console.log(`Este número é impar`);
+    }*/
+    let parImpar = function(num:number) : boolean{
+    if (num %2 == 0)
+        return true
+    else
+        return false
+    }
+}
